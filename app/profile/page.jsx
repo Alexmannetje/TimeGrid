be67419ba95016430f772128b8d0df7e0c42069e
Alexmannetje <alexander.Mannaet@hotmail.com>
2024-05-06 18:37:28 +0200

    .

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,24 +1,10 @@
 "use client";
 
 import NavBar from "@/components/navbar";
-import Modal from "@/components/modal";
 import { Fragment, useState } from "react";
 
 export default function Home() {
-  const [showModal, setShowModal] = useState(false);
-
-  const toggleModal = () => {
-    setShowModal(!showModal);
-  };
-
   return (
-    <Fragment>
-      <NavBar />
-      <button onClick={toggleModal}>
-      </button>
-      <Modal isVisible={showModal} onClose={() => setShowModal(false)}>
-
-      </Modal>
-    </Fragment>
+    <NavBar />
   );
 }
